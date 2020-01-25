c02a40e4d647df915c96ea89770a3ac00ac79655
ArtGooner <artstop92@gmail.com>
2020-01-25 17:43:32 +0300

    Change model structure

--- config/microservice.pb.go
+++ config/microservice.pb.go
@@ -28,7 +28,9 @@ type User struct {
 	Id                   int32    `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
 	Name                 string   `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
 	Surname              string   `protobuf:"bytes,3,opt,name=surname,proto3" json:"surname,omitempty"`
-	Age                  int32    `protobuf:"varint,4,opt,name=age,proto3" json:"age,omitempty"`
+	Email                string   `protobuf:"bytes,4,opt,name=email,proto3" json:"email,omitempty"`
+	Age                  int32    `protobuf:"varint,5,opt,name=age,proto3" json:"age,omitempty"`
+	PasswordHash         []byte   `protobuf:"bytes,6,opt,name=passwordHash,proto3" json:"passwordHash,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -80,6 +82,13 @@ func (m *User) GetSurname() string {
 	return ""
 }
 
+func (m *User) GetEmail() string {
+	if m != nil {
+		return m.Email
+	}
+	return ""
+}
+
 func (m *User) GetAge() int32 {
 	if m != nil {
 		return m.Age
@@ -87,9 +96,16 @@ func (m *User) GetAge() int32 {
 	return 0
 }
 
+func (m *User) GetPasswordHash() []byte {
+	if m != nil {
+		return m.PasswordHash
+	}
+	return nil
+}
+
 type Account struct {
 	Email                string   `protobuf:"bytes,1,opt,name=email,proto3" json:"email,omitempty"`
-	PasswordHash         string   `protobuf:"bytes,2,opt,name=passwordHash,proto3" json:"passwordHash,omitempty"`
+	Password             string   `protobuf:"bytes,2,opt,name=password,proto3" json:"password,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -127,9 +143,9 @@ func (m *Account) GetEmail() string {
 	return ""
 }
 
-func (m *Account) GetPasswordHash() string {
+func (m *Account) GetPassword() string {
 	if m != nil {
-		return m.PasswordHash
+		return m.Password
 	}
 	return ""
 }
@@ -142,20 +158,22 @@ func init() {
 func init() { proto.RegisterFile("microservice.proto", fileDescriptor_9cc2ddde859f443c) }
 
 var fileDescriptor_9cc2ddde859f443c = []byte{
-	// 203 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x54, 0x8f, 0x3f, 0x4f, 0x87, 0x30,
-	0x14, 0x45, 0xe5, 0x7f, 0x78, 0x12, 0x35, 0x2f, 0x0e, 0x8d, 0x13, 0xe9, 0xc4, 0x84, 0x89, 0xee,
-	0x26, 0xc4, 0xc5, 0x19, 0xa3, 0x7b, 0x2d, 0x4f, 0x68, 0x22, 0x2d, 0x69, 0x8b, 0x7e, 0x7d, 0x43,
-	0x81, 0xe1, 0xb7, 0xdd, 0x7b, 0x92, 0xf6, 0xbe, 0x03, 0x38, 0x2b, 0x69, 0x8d, 0x23, 0xfb, 0xab,
-	0x24, 0xb5, 0x8b, 0x35, 0xde, 0x60, 0x2e, 0x8d, 0xfe, 0x56, 0x23, 0xff, 0x84, 0xf4, 0xc3, 0x91,
-	0xc5, 0x1b, 0x88, 0xd5, 0xc0, 0xa2, 0x3a, 0x6a, 0xb2, 0x3e, 0x56, 0x03, 0x22, 0xa4, 0x5a, 0xcc,
-	0xc4, 0xe2, 0x3a, 0x6a, 0xca, 0x3e, 0x64, 0x64, 0x50, 0xb8, 0xd5, 0x06, 0x9c, 0x04, 0x7c, 0x56,
-	0xbc, 0x83, 0x44, 0x8c, 0xc4, 0xd2, 0xf0, 0x7c, 0x8b, 0xfc, 0x15, 0x8a, 0x4e, 0x4a, 0xb3, 0x6a,
-	0x8f, 0xf7, 0x90, 0xd1, 0x2c, 0xd4, 0x4f, 0xf8, 0xbd, 0xec, 0xf7, 0x82, 0x1c, 0xaa, 0x45, 0x38,
-	0xf7, 0x67, 0xec, 0xf0, 0x26, 0xdc, 0x74, 0x0c, 0x5d, 0xb0, 0xa7, 0x17, 0xb8, 0xee, 0x56, 0x3f,
-	0xbd, 0xef, 0x97, 0xe3, 0x23, 0x54, 0x5b, 0x25, 0xed, 0x95, 0x14, 0x9e, 0xf0, 0xb6, 0xdd, 0x25,
-	0xda, 0x63, 0xe9, 0xa1, 0x3a, 0xc1, 0xa6, 0xc4, 0xaf, 0xbe, 0xf2, 0xe0, 0xfa, 0xfc, 0x1f, 0x00,
-	0x00, 0xff, 0xff, 0xfe, 0x6e, 0x00, 0x91, 0x01, 0x01, 0x00, 0x00,
+	// 226 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x54, 0x90, 0x41, 0x4b, 0xc4, 0x30,
+	0x14, 0x84, 0x4d, 0xb7, 0xed, 0xea, 0x33, 0xa8, 0x3c, 0x3c, 0x84, 0x3d, 0x95, 0x9c, 0x7a, 0xaa,
+	0xa0, 0x47, 0x41, 0xd8, 0x9b, 0xe7, 0x8a, 0x3f, 0x20, 0xa6, 0xcf, 0xdd, 0x80, 0x4d, 0x4a, 0x92,
+	0xea, 0xdf, 0xf0, 0x27, 0x4b, 0xd3, 0x56, 0xdd, 0xdb, 0xcc, 0x07, 0x33, 0xc9, 0x3c, 0xc0, 0xde,
+	0x68, 0xef, 0x02, 0xf9, 0x4f, 0xa3, 0xa9, 0x19, 0xbc, 0x8b, 0x0e, 0x4b, 0xed, 0xec, 0xbb, 0x39,
+	0xc8, 0x6f, 0x06, 0xf9, 0x6b, 0x20, 0x8f, 0x57, 0x90, 0x99, 0x4e, 0xb0, 0x8a, 0xd5, 0x45, 0x9b,
+	0x99, 0x0e, 0x11, 0x72, 0xab, 0x7a, 0x12, 0x59, 0xc5, 0xea, 0x8b, 0x36, 0x69, 0x14, 0xb0, 0x0d,
+	0xa3, 0x4f, 0x78, 0x93, 0xf0, 0x6a, 0xf1, 0x16, 0x0a, 0xea, 0x95, 0xf9, 0x10, 0x79, 0xe2, 0xb3,
+	0xc1, 0x1b, 0xd8, 0xa8, 0x03, 0x89, 0x22, 0x95, 0x4e, 0x12, 0x25, 0xf0, 0x41, 0x85, 0xf0, 0xe5,
+	0x7c, 0xf7, 0xac, 0xc2, 0x51, 0x94, 0x15, 0xab, 0x79, 0x7b, 0xc2, 0xe4, 0x23, 0x6c, 0xf7, 0x5a,
+	0xbb, 0xd1, 0xc6, 0xbf, 0x5a, 0xf6, 0xbf, 0x76, 0x07, 0xe7, 0x6b, 0x60, 0xf9, 0xde, 0xaf, 0xbf,
+	0x7f, 0x82, 0xcb, 0x69, 0xce, 0xcb, 0x3c, 0x16, 0xef, 0x80, 0xef, 0xc7, 0x78, 0x24, 0x1b, 0x8d,
+	0x56, 0x91, 0xf0, 0xba, 0x99, 0x77, 0x37, 0xcb, 0x0b, 0x3b, 0xbe, 0x82, 0x29, 0x25, 0xcf, 0xde,
+	0xca, 0x74, 0x9e, 0x87, 0x9f, 0x00, 0x00, 0x00, 0xff, 0xff, 0x68, 0xdf, 0x66, 0xf8, 0x34, 0x01,
+	0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -166,72 +184,72 @@ var _ grpc.ClientConn
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion4
 
-// AuthServiceClient is the client API for AuthService service.
+// UserServiceClient is the client API for UserService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type AuthServiceClient interface {
+type UserServiceClient interface {
 	Authenticate(ctx context.Context, in *Account, opts ...grpc.CallOption) (*User, error)
 }
 
-type authServiceClient struct {
+type userServiceClient struct {
 	cc *grpc.ClientConn
 }
 
-func NewAuthServiceClient(cc *grpc.ClientConn) AuthServiceClient {
-	return &authServiceClient{cc}
+func NewUserServiceClient(cc *grpc.ClientConn) UserServiceClient {
+	return &userServiceClient{cc}
 }
 
-func (c *authServiceClient) Authenticate(ctx context.Context, in *Account, opts ...grpc.CallOption) (*User, error) {
+func (c *userServiceClient) Authenticate(ctx context.Context, in *Account, opts ...grpc.CallOption) (*User, error) {
 	out := new(User)
-	err := c.cc.Invoke(ctx, "/config.AuthService/Authenticate", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/config.UserService/Authenticate", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// AuthServiceServer is the server API for AuthService service.
-type AuthServiceServer interface {
+// UserServiceServer is the server API for UserService service.
+type UserServiceServer interface {
 	Authenticate(context.Context, *Account) (*User, error)
 }
 
-// UnimplementedAuthServiceServer can be embedded to have forward compatible implementations.
-type UnimplementedAuthServiceServer struct {
+// UnimplementedUserServiceServer can be embedded to have forward compatible implementations.
+type UnimplementedUserServiceServer struct {
 }
 
-func (*UnimplementedAuthServiceServer) Authenticate(ctx context.Context, req *Account) (*User, error) {
+func (*UnimplementedUserServiceServer) Authenticate(ctx context.Context, req *Account) (*User, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Authenticate not implemented")
 }
 
-func RegisterAuthServiceServer(s *grpc.Server, srv AuthServiceServer) {
-	s.RegisterService(&_AuthService_serviceDesc, srv)
+func RegisterUserServiceServer(s *grpc.Server, srv UserServiceServer) {
+	s.RegisterService(&_UserService_serviceDesc, srv)
 }
 
-func _AuthService_Authenticate_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _UserService_Authenticate_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Account)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(AuthServiceServer).Authenticate(ctx, in)
+		return srv.(UserServiceServer).Authenticate(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/config.AuthService/Authenticate",
+		FullMethod: "/config.UserService/Authenticate",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(AuthServiceServer).Authenticate(ctx, req.(*Account))
+		return srv.(UserServiceServer).Authenticate(ctx, req.(*Account))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-var _AuthService_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "config.AuthService",
-	HandlerType: (*AuthServiceServer)(nil),
+var _UserService_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "config.UserService",
+	HandlerType: (*UserServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "Authenticate",
-			Handler:    _AuthService_Authenticate_Handler,
+			Handler:    _UserService_Authenticate_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
